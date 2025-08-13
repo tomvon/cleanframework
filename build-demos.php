c757ecc2e47bbef80e76369fddd28d4421e1752b
Tom Von Lahndorff <tomvonl@gmail.com>
2025-08-12 23:13:42 -0400

    demo update

--- build-demos.php
+++ build-demos.php
@@ -86,21 +86,21 @@ if ($failures > 0) {
  * Process HTML for GitHub compatibility
  */
 function processHtml($html) {
-    // Fix stylesheet paths
-    $html = preg_replace('/href=["\']style\.css(\?[^"\']*)?["\']/i', 'href="../style.css"', $html);
+    // Generate cache-busting timestamp - using date format for htmlpreview.github.io compatibility
+    $cacheBuster = date('YmdHis');
     
-    // Fix main.js path  
-    $html = preg_replace('/src=["\']main\.js["\']/i', 'src="../main.js"', $html);
+    // Fix stylesheet paths with cache busting - using simple format
+    $html = preg_replace('/href=["\']style\.css(\?[^"\']*)?["\']/i', "href=\"../style.css?$cacheBuster\"", $html);
     
-    // Fix component paths
-    $html = preg_replace('/src=["\']components\/([^"\']+)["\']/i', 'src="../components/$1"', $html);
+    // Fix main.js path with cache busting
+    $html = preg_replace('/src=["\']main\.js["\']/i', "src=\"../main.js?$cacheBuster\"", $html);
+    
+    // Fix component paths with cache busting  
+    $html = preg_replace('/src=["\']components\/([^"\']+)["\']/i', "src=\"../components/$1?$cacheBuster\"", $html);
     
     // Fix any absolute paths that might break
-    $html = preg_replace('/href=["\']\/([^"\']+)["\']/i', 'href="../$1"', $html);
-    $html = preg_replace('/src=["\']\/([^"\']+)["\']/i', 'src="../$1"', $html);
-    
-    // Remove cache-busting parameters
-    $html = preg_replace('/\?v=\d+/i', '', $html);
+    $html = preg_replace('/href=["\']\/([^"\']+)["\']/i', "href=\"../$1\"", $html);
+    $html = preg_replace('/src=["\']\/([^"\']+)["\']/i', "src=\"../$1\"", $html);
     
     // Add Font Awesome CDN if not present
     if (strpos($html, 'font-awesome') === false) {
@@ -110,8 +110,14 @@ function processHtml($html) {
     
     // Add generation info
     $timestamp = date('Y-m-d H:i:s T');
-    $comment = "    <!-- Generated: $timestamp by CleanFramework Demo Builder -->\n";
+    $comment = "    <!-- Generated: $timestamp by CleanFramework Demo Builder (cache: $cacheBuster) -->\n";
     $html = str_replace('</head>', $comment . '</head>', $html);
+    
+    // Add cache-busting meta tag to help with browser caching
+    $metaTag = '    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">' . "\n";
+    $metaTag .= '    <meta http-equiv="Pragma" content="no-cache">' . "\n";  
+    $metaTag .= '    <meta http-equiv="Expires" content="0">' . "\n";
+    $html = str_replace('</head>', $metaTag . '</head>', $html);
     
     // Clean up any remaining localhost references
     $html = str_replace('http://localhost:8848/', '../', $html);
